4ea74412a37634227654d51d75d87b34e1c53ccf
captainsloths <alex.eiselstein@gmail.com>
2025-09-08 23:19:24 -0500

    added sys.argv for argument usage

--- main.py
+++ main.py
@@ -2,30 +2,29 @@ from stats import get_word_count
 from stats import get_times_character_repeats
 from stats import get_book_text
 from stats import get_report
+import sys
+
 
 def main():
-    book_path = "./books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    # Use the second argument in sys.argv as the path to the book file
+    book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
-    word_count = get_word_count(book_text)
     chars_dict = get_times_character_repeats(book_text)
 
-    # num_words = get_word_count(book_text)
-    # print(f"{num_words} words found in the document")
-    
-    # character_count = get_times_character_repeats(book_text)
-    # print(character_count)
     print("===== BOOK BOT =====")
     print(f"Analyzing book found at {book_path}")
     print("----- Word Count -----")
-    
     num_words = get_word_count(book_text)
     print(f"Found {num_words} total words")
-    
-
     print("----- Character Count -----")
     get_report(chars_dict)
     print("===== END =====")
 
 
 main()
-
--- stats.py
+++ stats.py
@@ -1,9 +1,11 @@
 def get_book_text(book_path):
-    with open(book_path, 'r') as f:
+    with open(book_path, 'r', encoding='utf-8') as f:
         return f.read()
+
 
 def get_word_count(book_text):
     return len(book_text.split())
+
 
 def get_times_character_repeats(book_text):
     character_count = {}
@@ -14,13 +16,13 @@ def get_times_character_repeats(book_text):
             character_count[char] += 1
         else:
             character_count[char] = 1
-            
+
     return character_count
 
+
 def get_report(chars_dict):
-    sorted_report = dict(sorted(chars_dict.items(), key = lambda item: item[1], reverse=True))
+    sorted_report = dict(
+        sorted(chars_dict.items(), key=lambda item: item[1], reverse=True))
     for report in sorted_report:
-        if report.isalpha():
+        if report.isalpha() and report.isascii():
             print(f"{report}: {sorted_report[report]}")
-
-
